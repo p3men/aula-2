6ac5acfcfc0b2fc30a9bbcdbdec7526b8916a311
Pablo Marcelino Mariano <00001116780598sp@aluno.educacao.sp.gov.br>
2024-05-22 17:36:36 -0300

    first commit

---
+++ exercicio3.py
@@ -0,0 +1,5 @@
+quantidade_desejada = int(input("Digite a quantidade desejada: "))
+if quantidade_desejada >=10:
+    print("Produto disponivel em estoque. Aproveite!")
+else:
+    print("Produto indisponivel no momento. Volte mais tarde!")
---
+++ exercicio4.py
@@ -0,0 +1,5 @@
+nota_final = float(input("Digite a nota final: "))
+if nota_final >= 7:
+    print("Parabens! Voce foi aprovado.")
+else:
+    print("Infelizmente, voce nao foi aprovado. Estude mais!")
